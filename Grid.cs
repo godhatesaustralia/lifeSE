161045777b1180b85b90d33b820574b699eca802
godhatesaustralia <asybad69@gmail.com>
2023-10-18 23:31:13 -0400

    Add project files.

---
+++ Grid.cs
@@ -0,0 +1,124 @@
+﻿using Sandbox.ModAPI.Ingame;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using VRageMath;
+
+namespace IngameScript
+{
+    public class Grid
+    {
+        bool customSeed;
+
+        int arraySizeX, arraySizeY, iterator = Constants.xmin;
+        Dictionary<Vector2, bool>
+            buffer = new Dictionary<Vector2, bool>(),
+            bufferOld = new Dictionary<Vector2, bool>();
+        Random rand = new Random();
+
+        public Grid(MyGridProgram program, bool custom = false, Vector2? size = null) 
+        {
+            customSeed = custom;
+            arraySizeX = (int)(size.HasValue ? size.Value.X : Constants.xrange);
+            arraySizeY = (int)(size.HasValue ? size.Value.Y : Constants.yrange);
+            for (int i = Constants.xmin; i < Constants.xmax; ++i)
+                for (int j = Constants.ymin; j < Constants.ymax; ++j)
+            {
+                var c = new Vector2(i, j);    
+                if (!customSeed)
+                    if (rand.NextDouble() < 0.29)
+                        buffer.Add(c, true);
+                    else
+                        buffer.Add(c, false);
+                if (buffer[c])
+                    program.Echo($"Cell born at {c}");
+            }
+        }
+
+        public Dictionary<Vector2, bool> Buffer { get { return buffer; } }
+        Vector2 WrapCoords(ref Vector2 v)
+        {
+            v.X = v.X > arraySizeX ? 0 : v.X;
+            v.X = v.X < 0 ? arraySizeX : v.X;
+            v.Y = v.Y > arraySizeY ? 0 : v.Y;
+            v.Y = v.Y < 0 ? arraySizeY : v.Y;
+            return v;
+        }
+
+        public bool Get(Vector2 v)
+        {
+            v = WrapCoords(ref v);
+            return buffer[v];
+        }
+
+        public void Set(Vector2 v, bool b = true)
+        {
+            v = WrapCoords(ref v);
+            buffer[v] = b;
+        }
+
+        public void Copy()
+        {
+            bufferOld = buffer;
+        }
+
+        public void Update(int cycles = 0)
+        {
+            if (cycles == 0) // this is too much for it to do in a single run. gonna leave it in tho
+                updateLoop(buffer.Keys); 
+            else
+            {
+                var array = new List<Vector2>();
+                for(int i = iterator; i < iterator + cycles; ++i)
+                    for (int j = Constants.ymin; j < Constants.ymax; ++j)
+                        array.Add(new Vector2(i, j));
+                    updateLoop(array);
+
+                iterator++;
+            }
+            if (iterator >= Constants.xmax) iterator = Constants.xmin;
+        }
+
+        void updateLoop(IEnumerable<Vector2> group)
+        {
+            foreach (var position in group)
+            {
+                // theres proabbly a WAYY better way of doiing this
+                var x = position.X;
+                var y = position.Y;
+                var adj = new Vector2[8];
+                var ret = new List<bool>();
+                adj[0] = new Vector2(x - 1, y - 1);
+                adj[1] = new Vector2(x - 1, y + 1);
+                adj[2] = new Vector2(x - 1, y);
+                adj[3] = new Vector2(x, y - 1);
+                adj[4] = new Vector2(x, y + 1);
+                adj[5] = new Vector2(x + 1, y - 1);
+                adj[6] = new Vector2(x + 1, y);
+                adj[7] = new Vector2(x + 1, y + 1);
+                for (int i = 0; i < 8; ++i)
+                    WrapCoords(ref adj[i]);
+                var count = 0;
+
+                foreach (var v in adj)
+                    if (bufferOld.ContainsKey(v))
+                        if(bufferOld[v])
+                            ++count;
+
+                    if (count == 3)
+                        // if cell have 3 neighbor it alive
+                        buffer[position] = true;
+                    else if (bufferOld[position] && count == 2)
+                        // if cell have 2 neighbor and it alr alive it stay alive
+                        buffer[position] = true;
+                    else
+                        // otherwise it dood
+                        buffer[position] = false;               
+
+            }          
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,143 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.Achievements;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Xml.Serialization;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        public MyIni ini;
+        public Random rand;
+        Grid grid;
+        public IMyTextSurface drawSurface;
+        public MyGridProgram program;
+        public bool customSeed;
+        int runs = 0;
+        double runtimeMS, worst;
+        string cdtag = "[CUSTOM]";
+
+        // Custom data just
+        // [CUSTOM]
+        // (X1, Y1)
+        // (X2, Y2)
+        // ...
+        // (XN, YN)
+        // where these are spots youre setting to true
+
+        public Program()
+        {            
+            program = this;
+            rand = new Random();
+            ini = new MyIni();
+            Runtime.UpdateFrequency = UpdateFrequency.Update1;
+            var blcok = GridTerminalSystem.GetBlockWithName("EVT LCD Console CGL");
+            drawSurface = ((IMyTextSurfaceProvider)blcok).GetSurface(0);
+            customSeed = blcok.CustomData.Contains(cdtag);
+            Vector2 screen;
+            if (drawSurface != null )
+            {
+                drawSurface.ContentType = ContentType.SCRIPT;
+                drawSurface.ScriptBackgroundColor = new Color(7, 7, 7, 255);
+                screen = drawSurface.SurfaceSize;
+            }
+            if (customSeed)
+            {
+                grid = new Grid(this, true);
+                string cd = blcok.CustomData;
+                var array = cd.Split('\n');
+                foreach (var line in array)
+                {
+                    if (line.Contains(cdtag)) 
+                        continue;
+                    line.Trim(new char[] { '(', ')' });
+                    var coords = line.Split(',');
+                    if (coords.Length != 2) 
+                        continue;
+                    grid.Set(new Vector2(float.Parse(coords[0]), float.Parse(coords[1])));
+                }
+            }
+            else
+            {
+                grid = new Grid(this);
+                // Me.CustomData = "";
+            }
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            runtimeMS = Runtime.TimeSinceLastRun.TotalMilliseconds;
+            worst = runtimeMS > worst ? runtimeMS : worst;
+            var echoString = $"{runs} cycles\n";
+            var tickModulus = runs % Constants.xrange;
+            if (runs <= Constants.xrange)
+            {
+                ++runs;
+                if (tickModulus == 0)
+                    grid.Copy();
+                return;
+            }
+
+            else if (tickModulus != 0)
+                grid.Update(1);
+
+
+            else if (tickModulus == 0)
+            {
+                grid.Copy();
+                Me.CustomData = "";
+
+                echoString += $"Tick {tickModulus}";
+
+                var frame = drawSurface.DrawFrame();
+                foreach (var c in grid.Buffer)
+                    if (c.Value)
+                        frame.Add(new MySprite(SpriteType.TEXTURE, "SquareSimple", c.Key * Constants.sizeMPLR, new Vector2(Constants.sizeMPLR, Constants.sizeMPLR), Constants.display, null, TextAlignment.CENTER, 0f));
+
+                frame.Add(new MySprite(SpriteType.TEXT, $"UPDATE {runs/Constants.xrange}             GAME OF LIFE", Constants.textPos, null, Constants.display, "VCRBold", TextAlignment.CENTER, 1f));
+                frame.Dispose();
+            }
+            Echo(echoString + $"\nRuntime: {Runtime.TimeSinceLastRun.TotalMilliseconds} ms");
+            ++runs;
+
+        }
+    }
+    public static class Constants
+    {
+        // for entertainment hub block
+        // was going to do logic automatically determining how to set this up
+        // but it really relies on what subjectively looks "good" so thatd be a waste
+        public static int
+            xmin = 1,
+            xmax = 127,
+            ymin = 27,
+            ymax = 119,
+            xrange = xmax - xmin,
+            yrange = ymax - ymin;
+        public static float sizeMPLR = 8;
+        public static Vector2 
+            size = new Vector2(xrange, yrange),
+            textPos = new Vector2(508, 752);
+        public static Color display = new Color(250, 250, 250, 255);
+
+
+    }
+}
